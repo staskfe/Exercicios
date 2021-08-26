8b85c89efe940711944d269aba9c74cc9b4a1389
stasfe <felipestaskoviak@gmail.com>
2021-08-26 16:48:36 -0300

    Adicionar arquivos de projeto.

---
+++ Treinamento/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Treinamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
